384e6721ea82b064d53544b353b996cf41a4cf22
notauserx <notauserx@users.noreply.github.com>
2022-08-22 08:46:24 +0600

    uncomment test since mysql is working now

--- services/bookingservice/src/test/java/com/practicalddd/cargotracker/bookingservice/BookingserviceApplicationTests.java
+++ services/bookingservice/src/test/java/com/practicalddd/cargotracker/bookingservice/BookingserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookingserviceApplicationTests {
 
-	//@Test
-	//void contextLoads() {
-	//}
+	@Test
+	void contextLoads() {
+	}
 
 }
