f2ce6337635aa331ec581b21242416f41adb8e03
notauserx <notauserx@users.noreply.github.com>
2022-08-24 23:00:51 +0600

    initialize routing service

---
+++ services/routingservice/src/main/java/com/practicalddd/cargotracker/routingservice/RoutingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.practicalddd.cargotracker.routingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingserviceApplication.class, args);
+	}
+
+}
