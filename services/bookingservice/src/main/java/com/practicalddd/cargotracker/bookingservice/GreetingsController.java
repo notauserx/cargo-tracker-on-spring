937a943fc37a2b2728450523db8173c227ceab10
notauserx <notauserx@users.noreply.github.com>
2022-08-24 14:07:09 +0600

    update dockerfile

--- services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/GreetingsController.java
+++ services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/GreetingsController.java
@@ -4,10 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greetings")
 public class GreetingsController {
     @RequestMapping("/")
     public String hello()
     {
-        return "Hello world";
+        return "Hello world. Again";
+    }
+
+    @RequestMapping("/name")
+    public String hello(String name)
+    {
+        return "Hello world," + name;
     }
 }
