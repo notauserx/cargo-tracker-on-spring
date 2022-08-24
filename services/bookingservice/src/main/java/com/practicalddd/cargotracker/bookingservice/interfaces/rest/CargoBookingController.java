b9d1b88afc71bc5b5f0149430ffbf9bf824768d8
notauserx <notauserx@users.noreply.github.com>
2022-08-24 11:43:34 +0600

    add api endpoint

--- services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/interfaces/rest/CargoBookingController.java
+++ services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/interfaces/rest/CargoBookingController.java
@@ -1,9 +1,23 @@
 package com.practicalddd.cargotracker.bookingservice.interfaces.rest;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.practicalddd.cargotracker.bookingservice.application.internal.queryservices.CargoBookingQueryService;
+import com.practicalddd.cargotracker.bookingservice.domain.model.aggregates.BookingId;
+import com.practicalddd.cargotracker.bookingservice.domain.model.aggregates.Cargo;
+import org.springframework.web.bind.annotation.*;
 
-@Controller // This means that this class is a Controller
+@RestController // This means that this class is a Controller
 @RequestMapping("/cargobooking") // The URI of the API
 public class CargoBookingController {
+    private CargoBookingQueryService cargoBookingQueryService;
+
+    /**
+     * GET method to retrieve a Cargo
+     * @param bookingId
+     * @return
+     */
+    @GetMapping("/findCargo")
+    @ResponseBody
+    public Cargo findByBookingId(@RequestParam("bookingId") String bookingId){
+        return cargoBookingQueryService.find(new BookingId(bookingId));
+    }
 }
