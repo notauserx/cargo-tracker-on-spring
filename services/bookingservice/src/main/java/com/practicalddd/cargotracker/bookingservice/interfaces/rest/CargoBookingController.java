1f4167eacfb3d8a0f3bbde57dd59e23b4cadc030
notauserx <notauserx@users.noreply.github.com>
2022-08-24 10:56:38 +0600

    wip add bookcargo controller

---
+++ services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/interfaces/rest/CargoBookingController.java
@@ -0,0 +1,9 @@
+package com.practicalddd.cargotracker.bookingservice.interfaces.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // This means that this class is a Controller
+@RequestMapping("/cargobooking") // The URI of the API
+public class CargoBookingController {
+}
---
+++ services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/interfaces/rest/transform/BookCargoCommandDTOAssembler.java
@@ -0,0 +1,9 @@
+package com.practicalddd.cargotracker.bookingservice.interfaces.rest.transform;
+
+import com.practicalddd.cargotracker.bookingservice.interfaces.rest.dto.BookCargoResource;
+
+/**
+ * Assembler class to convert the Book Cargo Resource Data to the Book Cargo Model
+ */
+public class BookCargoCommandDTOAssembler {
+}
