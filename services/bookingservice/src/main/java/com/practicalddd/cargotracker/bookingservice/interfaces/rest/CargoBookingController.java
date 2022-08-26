f53d7d5f596093fa29c2d028b9894e26dd1de0b9
notauserx <notauserx@users.noreply.github.com>
2022-08-26 11:20:28 +0600

    add bookCargo endpoint

--- services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/interfaces/rest/CargoBookingController.java
+++ services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/interfaces/rest/CargoBookingController.java
@@ -1,14 +1,34 @@
 package com.practicalddd.cargotracker.bookingservice.interfaces.rest;
 
+import com.practicalddd.cargotracker.bookingservice.application.internal.commandservices.CargoBookingCommandService;
 import com.practicalddd.cargotracker.bookingservice.application.internal.queryservices.CargoBookingQueryService;
 import com.practicalddd.cargotracker.bookingservice.domain.model.aggregates.BookingId;
 import com.practicalddd.cargotracker.bookingservice.domain.model.aggregates.Cargo;
+import com.practicalddd.cargotracker.bookingservice.interfaces.rest.dto.BookCargoResource;
+import com.practicalddd.cargotracker.bookingservice.interfaces.rest.transform.BookCargoCommandDTOAssembler;
 import org.springframework.web.bind.annotation.*;
 
 @RestController // This means that this class is a Controller
 @RequestMapping("/cargobooking") // The URI of the API
 public class CargoBookingController {
     private CargoBookingQueryService cargoBookingQueryService;
+    private CargoBookingCommandService cargoBookingCommandService;
+
+    public CargoBookingController(CargoBookingQueryService cargoBookingQueryService,
+                                  CargoBookingCommandService cargoBookingCommandService) {
+        this.cargoBookingQueryService = cargoBookingQueryService;
+        this.cargoBookingCommandService = cargoBookingCommandService;
+    }
+
+    @PostMapping
+    @ResponseBody
+    public BookingId bookCargo(@RequestBody BookCargoResource bookCargoResource) {
+        System.out.println("***Cargo booked***" + bookCargoResource.getBookingAmount());
+        BookingId bookingId = cargoBookingCommandService.BookCargo(
+                BookCargoCommandDTOAssembler.toCommandFromDTO(bookCargoResource)
+        );
+        return bookingId;
+    }
 
     /**
      * GET method to retrieve a Cargo
