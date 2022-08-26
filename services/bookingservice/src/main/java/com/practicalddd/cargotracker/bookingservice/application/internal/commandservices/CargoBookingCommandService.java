f135b9fc404a09c671ad02301ecfcd1a9099e8c8
notauserx <notauserx@users.noreply.github.com>
2022-08-26 12:00:45 +0600

    add method assignRouteToCargo

--- services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/application/internal/commandservices/CargoBookingCommandService.java
+++ services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/application/internal/commandservices/CargoBookingCommandService.java
@@ -1,8 +1,11 @@
 package com.practicalddd.cargotracker.bookingservice.application.internal.commandservices;
 
+import com.practicalddd.cargotracker.bookingservice.application.internal.outboundservices.acl.ExternalCargoRoutingService;
 import com.practicalddd.cargotracker.bookingservice.domain.model.aggregates.BookingId;
 import com.practicalddd.cargotracker.bookingservice.domain.model.aggregates.Cargo;
 import com.practicalddd.cargotracker.bookingservice.domain.model.commands.BookCargoCommand;
+import com.practicalddd.cargotracker.bookingservice.domain.model.commands.RouteCargoCommand;
+import com.practicalddd.cargotracker.bookingservice.domain.model.valueobjects.CargoItinerary;
 import com.practicalddd.cargotracker.bookingservice.infrastructure.repositories.CargoRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +14,12 @@ import java.util.UUID;
 @Service
 public class CargoBookingCommandService {
     private CargoRepository cargoRepository;
+    private ExternalCargoRoutingService externalCargoRoutingService;
 
-    public CargoBookingCommandService(CargoRepository cargoRepository) {
+    public CargoBookingCommandService(CargoRepository cargoRepository,
+                                      ExternalCargoRoutingService externalCargoRoutingService) {
         this.cargoRepository = cargoRepository;
+        this.externalCargoRoutingService = externalCargoRoutingService;
     }
 
     public BookingId BookCargo(BookCargoCommand bookCargoCommand) {
@@ -24,5 +30,21 @@ public class CargoBookingCommandService {
 
         cargoRepository.save(cargo);
         return new BookingId(bookCargoCommand.getBookingId());
+    }
+    /**
+     * Service Command method to assign a route to a Cargo
+     * @param routeCargoCommand
+     */
+
+    public void assignRouteToCargo(RouteCargoCommand routeCargoCommand){
+        System.out.println("Route Cargo command"+routeCargoCommand.getCargoBookingId());
+        Cargo cargo = cargoRepository.findByBookingId(
+                new BookingId(routeCargoCommand.getCargoBookingId()));
+        CargoItinerary cargoItinerary = externalCargoRoutingService
+                .fetchRouteForSpecification(cargo.getRouteSpecification());
+
+        cargo.assignToRoute(cargoItinerary);
+        cargoRepository.save(cargo);
+
     }
 }
