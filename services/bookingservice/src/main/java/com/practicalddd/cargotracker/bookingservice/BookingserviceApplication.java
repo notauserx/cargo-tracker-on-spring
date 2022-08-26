2fdd4bc794be27ce85ffa39407f831a183ba5632
notauserx <notauserx@users.noreply.github.com>
2022-08-26 11:59:08 +0600

    remove sqlinitialization auto config

--- services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/BookingserviceApplication.java
+++ services/bookingservice/src/main/java/com/practicalddd/cargotracker/bookingservice/BookingserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 
-@SpringBootApplication(exclude = SqlInitializationAutoConfiguration.class)
+@SpringBootApplication
 public class BookingserviceApplication {
 
 	// http://localhost:8080/swagger-ui.html
